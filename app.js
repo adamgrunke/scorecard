818b01854dfdca8d0ecac307c462099e24bfffa3
adamgrunke <adam.grunke@gmail.com>
2019-10-10 12:45:07 -0700

    CORS issue. Need to resolve.

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-const title = React.createElement(
-    'h1',
-    {id: 'main-title', title: 'This is a title'},
-    'My react element'
+// const title = React.createElement(
+//     'h1',
+//     {id: 'main-title', title: 'This is a title'},
+//     'My react element'
+// );
+
+
+// const desc = React.createElement(
+    //     'p',
+    //     null,
+    //     'Created react node and rendered it into the DOM'
+    // );
+    
+console.log(title)
+const title = <h1>React Element</h1>
+const desc = <p>Created react node and rendered it into the DOM </p>
+
+const header = React.createElement(
+    'header',
+    null,
+    title,
+    desc
 );
-console.log("test test")
-console.log(title);
+
+ReactDOM.render(
+    header,
+    document.getElementById('root')
+);
